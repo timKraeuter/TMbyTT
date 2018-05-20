824c98c2cf4b809d6f087b711d5a7e2090738ed9
admin <admin@NB15-FH328.ha.bib.de>
2018-05-20 19:16:38 +0200

    test commit

--- src/main/java/turingmaschine/Band.java
+++ src/main/java/turingmaschine/Band.java
@@ -2,24 +2,25 @@ package turingmaschine;
 
 import java.util.List;
 
+//test kommenater
 public class Band {
-
-    private final List<Zeichen> inhalteDesBands;
-    // Löwe würde jetzt BigInteger nehmen :D
-    private final int positionDesSchreibLeseKopfes;
-
-
-    private Band(final List<Zeichen> inhalteDesBands, final int positionDesSchreibLeseKopfes) {
-        this.inhalteDesBands = inhalteDesBands;
-        this.positionDesSchreibLeseKopfes = positionDesSchreibLeseKopfes;
-    }
-
-    public static Band create(final List<Zeichen> inhalteDesBands, final int positionDesSchreibLeseKopfes) {
-        return new Band(inhalteDesBands, positionDesSchreibLeseKopfes);
-    }
-
-    public Band verarbeite(final Zeichen zuSchreibendesZeichen, final Lesekopfbewegung lesekopfBewegung) {
-        // Sonderfälle wenn die position bei 0 oder der Länge der Liste bzw. offset bei 1 ist.
-        throw new UnsupportedOperationException();
-    }
+	
+	private final List<Zeichen> inhalteDesBands;
+	// Löwe würde jetzt BigInteger nehmen :D
+	private final int positionDesSchreibLeseKopfes;
+	
+	
+	private Band(final List<Zeichen> inhalteDesBands, final int positionDesSchreibLeseKopfes) {
+		this.inhalteDesBands = inhalteDesBands;
+		this.positionDesSchreibLeseKopfes = positionDesSchreibLeseKopfes;
+	}
+	
+	public static Band create(final List<Zeichen> inhalteDesBands, final int positionDesSchreibLeseKopfes) {
+		return new Band(inhalteDesBands, positionDesSchreibLeseKopfes);
+	}
+	
+	public Band verarbeite(final Zeichen zuSchreibendesZeichen, final Lesekopfbewegung lesekopfBewegung) {
+		// Sonderfälle wenn die position bei 0 oder der Länge der Liste bzw. offset bei 1 ist.
+		throw new UnsupportedOperationException();
+	}
 }
