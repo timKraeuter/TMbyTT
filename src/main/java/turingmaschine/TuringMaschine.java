6e30c5303929977fb3071c600b907bcc769e72f2
timKraeuter <tim.kraeuterr@gmail.com>
2018-05-27 19:49:55 +0200

    Eine TM zum Kopieren von Zahlen auf ihrem Eingabeband zu ihrem Ausgabe band wurde implementiert. Siehe CopyNumber.feature

--- src/main/java/turingmaschine/TuringMaschine.java
+++ src/main/java/turingmaschine/TuringMaschine.java
@@ -7,177 +7,176 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TuringMaschine {
-	
-	private final Zustand startZustand;
-	private final Set<Zustand> endZustaende;
-	
-	private final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion;
-	
-	private final int anzahlDerBaender;
-	
-	private TuringMaschine(final Zustand startZustand, final Set<Zustand> endZustaende,
-			final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion,
-			final int anzahlDerBaender) {
-		this.startZustand = startZustand;
-		this.endZustaende = endZustaende;
-		this.ueberfuehrungsfunktion = ueberfuehrungsfunktion;
-		this.anzahlDerBaender = anzahlDerBaender;
-	}
-	
-	public static TuringMaschine create(final Zustand startZustand, final Set<Zustand> endZustaende,
-			final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion,
-			final int anzahlDerBaender) {
-		return new TuringMaschine(startZustand, endZustaende, ueberfuehrungsfunktion, anzahlDerBaender);
-	}
-	
-	// Wir gehen erstmal von deterministisch aus
-	// Hier wird wild ein Band erzeugt und so ein Gedöns.
-	public Set<Konfiguration> simuliere(final List<String> eingaben) {
-		
-		final List<Band> eingabeBaender = eingaben.stream().map(eingabe -> Band.create(eingabe)).collect(Collectors.toList());
-		final Konfiguration startConfig = Konfiguration.create(this.startZustand, eingabeBaender, this);
-		Set<Konfiguration> naechsteKonfigurationen = Collections.singleton(startConfig);
-		
-		final Set<Konfiguration> endKonfigurationen = new HashSet<>();
-		while (!naechsteKonfigurationen.isEmpty()) {
-			final HashSet<Konfiguration> neueNaechsteConfigs = new HashSet<>();
-			
-			for (final Konfiguration config : naechsteKonfigurationen) {
-				if (config.isEndKonfiguration()) {
-					endKonfigurationen.add(config);
-				} else {
-					neueNaechsteConfigs.addAll(this.step(config));
-				}
-			}
-			
-			naechsteKonfigurationen = neueNaechsteConfigs;
-		}
-		return endKonfigurationen;
-	}
-	
-	private Set<Konfiguration> step(final Konfiguration konfiguration) {
-		return this.ueberfuehrungsfunktion.stream().filter(e -> e.istPassendeUeberfuehrungZu(konfiguration))
-				.map(konfiguration::doUeberfuehrung).collect(Collectors.toSet());
-	}
-	
-	public boolean erkenntEingabe(final String eingabe) {
-		return !this.simuliere(Collections.singletonList(eingabe)).isEmpty();
-	}
-	
-	public void persistToFile(final String path) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public void loadFromFile(final String path) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public boolean isEndzustand(final Zustand moeglicherEndzustand) {
-		return this.endZustaende.contains(moeglicherEndzustand);
-	}
-	
-	public Set<Zeichen> getArbeitsalphabet() {
-		// final Set<Zeichen> arbeitsalphabet = this.ueberfuehrungsfunktion.stream()
-		// .flatMap(ElementDerUeberfuehrungsfunktion::getZuSchreibendeZeichen).collect(Collectors.toSet());
-		// arbeitsalphabet.addAll(this.getEingabealphabet());
-		// arbeitsalphabet.add(Blank.getInstance());
-		// return arbeitsalphabet;
-		return null;
-	}
-	
-	// TODO was bringt das Eingabealphabet und der Blank darf hier eigentlich nicht
-	// rein. siehe def. seite 4 und 5
-	public Set<Zeichen> getEingabealphabet() {
-		// return this.ueberfuehrungsfunktion.stream().map(ElementDerUeberfuehrungsfunktion::getEingabe)
-		// .collect(Collectors.toSet());
-		return null;
-	}
-	
-	/**
-	 * @return alle Zustände des Automaten einschließlich Anfangs- und Endzustand.
-	 */
-	public Set<Zustand> getZustaende() {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static TuringMaschinenBuilder builder() {
-		return TuringMaschinenBuilder.create();
-	}
-	
-	/**
-	 * Erstellt eine Turingmaschine, welche bei Ausführung die Bänder sum1 und sum2
-	 * addiert und dabei das Ergebnis in das result-Band schreibt.
-	 * 
-	 * @param sum1
-	 *            zu addierendes Band.
-	 * @param sum2
-	 *            zu addierendes Band.
-	 * @param result
-	 *            Band, auf welches bei Ausführung der TuringMaschine das Ergebnis
-	 *            geschrieben wird.
-	 * @return Additions-Turingmaschine.
-	 */
-	public static TuringMaschine createAdd(final Band sum1, final Band sum2, final Band result) {
-		return null;
-	}
-	
-	/**
-	 * Erstellt eine Turingmaschine, welche bei Ausführung die Bänder sub und min
-	 * voneinander subtrahiert und dabei das Ergebnis in das result-Band schreibt.
-	 * 
-	 * @param sub
-	 *            subtrahend-Band.
-	 * @param min
-	 *            minuend-Band.
-	 * @param result
-	 *            Band, auf welches bei Ausführung der TuringMaschine das Ergebnis
-	 *            geschrieben wird.
-	 * @return Subtraktions-Turingmaschine.
-	 */
-	public static TuringMaschine createSub(final Band sub, final Band min, final Band result) {
-		return null;
-	}
-	
-	/**
-	 * Erstellt eine Turingmaschine, welche bei Ausführung die Inhalte des
-	 * from-Bandes auf das into-Band schreibt.
-	 * 
-	 * @param from
-	 *            zu Kopierendes Band.
-	 * @param into
-	 *            Band in welches Kopiert werden soll.
-	 * @return Kopier-Turingmaschine.
-	 */
-	public static TuringMaschine createCopy(final Band from, final Band into) {
-		return null;
-	}
-	
-	/**
-	 * Erstellt eine Turingmaschine, welche die TuringMaschine tm solang ausführt,
-	 * bis das Condition-Band den Wert 0 hat.
-	 * 
-	 * @param condition
-	 *            zu überprüfende Condition. tm wird solang wiederholt ausgeführt,
-	 *            bis das Condition-Band den Wert 0 hat.
-	 * @param tm
-	 *            wiederholt auszuführende Turingmaschine.
-	 * @return While-Turingmaschine.
-	 */
-	public static TuringMaschine createWhile(final Band condition, final TuringMaschine tm) {
-		return null;
-	}
-	
-	/**
-	 * Erstellt eine Turingmaschine, welche die TuringMaschine t1 und t2
-	 * hintereinander ausführen kann.
-	 * 
-	 * @param t1
-	 *            erste auszuführende Turingmaschine.
-	 * @param t2
-	 *            zweite auszuführende Turingmaschine.
-	 * @return Sequenz-Turingmaschine.
-	 */
-	public static TuringMaschine createSeq(final TuringMaschine t1, final TuringMaschine t2) {
-		return null;
-	}
+
+    private final Zustand startZustand;
+    private final Set<Zustand> endZustaende;
+
+    private final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion;
+
+    private final int anzahlDerBaender;
+
+    private TuringMaschine(final Zustand startZustand, final Set<Zustand> endZustaende,
+                           final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion,
+                           final int anzahlDerBaender) {
+        this.startZustand = startZustand;
+        this.endZustaende = endZustaende;
+        this.anzahlDerBaender = anzahlDerBaender;
+        if (! parameterDerUeberfuhrungKorrekt(ueberfuehrungsfunktion)) {
+            throw new RuntimeException(String.format("Bei einer Anzahl von %s Bändern, muss die Überführungsfunktion immer komplett definiert sein", this.anzahlDerBaender));
+        }
+        this.ueberfuehrungsfunktion = ueberfuehrungsfunktion;
+    }
+
+    private boolean parameterDerUeberfuhrungKorrekt(Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion) {
+        return ueberfuehrungsfunktion.stream()
+                .allMatch(elementDerUeberfuehrungsfunktion -> elementDerUeberfuehrungsfunktion.getLesekopfBewegungen().size() == this.anzahlDerBaender &&
+                        elementDerUeberfuehrungsfunktion.getZuSchreibendeZeichen().size() == this.anzahlDerBaender &&
+                        elementDerUeberfuehrungsfunktion.getEingaben().size() == this.anzahlDerBaender);
+
+    }
+
+    public static TuringMaschine create(final Zustand startZustand, final Set<Zustand> endZustaende,
+                                        final Set<ElementDerUeberfuehrungsfunktion> ueberfuehrungsfunktion,
+                                        final int anzahlDerBaender) {
+        return new TuringMaschine(startZustand, endZustaende, ueberfuehrungsfunktion, anzahlDerBaender);
+    }
+
+    // Wir gehen erstmal von deterministisch aus
+    // Hier wird wild ein Band erzeugt und so ein Gedöns.
+    public Set<Konfiguration> simuliere(final List<String> eingaben) {
+
+        final List<Band> eingabeBaender = eingaben.stream().map(Band::create).collect(Collectors.toList());
+        final Konfiguration startConfig = Konfiguration.create(this.startZustand, eingabeBaender, this);
+        Set<Konfiguration> naechsteKonfigurationen = Collections.singleton(startConfig);
+
+        final Set<Konfiguration> endKonfigurationen = new HashSet<>();
+        while (!naechsteKonfigurationen.isEmpty()) {
+            final HashSet<Konfiguration> neueNaechsteConfigs = new HashSet<>();
+
+            for (final Konfiguration config : naechsteKonfigurationen) {
+                if (config.isEndKonfiguration()) {
+                    endKonfigurationen.add(config);
+                } else {
+                    neueNaechsteConfigs.addAll(this.step(config));
+                }
+            }
+
+            naechsteKonfigurationen = neueNaechsteConfigs;
+        }
+        return endKonfigurationen;
+    }
+
+    private Set<Konfiguration> step(final Konfiguration konfiguration) {
+        return this.ueberfuehrungsfunktion.stream().filter(e -> e.istPassendeUeberfuehrungZu(konfiguration))
+                .map(konfiguration::doUeberfuehrung).collect(Collectors.toSet());
+    }
+
+    public boolean erkenntEingabe(final String eingabe) {
+        return !this.simuliere(Collections.singletonList(eingabe)).isEmpty();
+    }
+
+    public void persistToFile(final String path) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void loadFromFile(final String path) {
+        throw new UnsupportedOperationException();
+    }
+
+    boolean isEndzustand(final Zustand moeglicherEndzustand) {
+        return this.endZustaende.contains(moeglicherEndzustand);
+    }
+
+    public Set<Zeichen> getArbeitsalphabet() {
+        // final Set<Zeichen> arbeitsalphabet = this.ueberfuehrungsfunktion.stream()
+        // .flatMap(ElementDerUeberfuehrungsfunktion::getZuSchreibendeZeichen).collect(Collectors.toSet());
+        // arbeitsalphabet.addAll(this.getEingabealphabet());
+        // arbeitsalphabet.add(Blank.getInstance());
+        // return arbeitsalphabet;
+        return null;
+    }
+
+    // TODO was bringt das Eingabealphabet und der Blank darf hier eigentlich nicht
+    // rein. siehe def. seite 4 und 5
+    public Set<Zeichen> getEingabealphabet() {
+        // return this.ueberfuehrungsfunktion.stream().map(ElementDerUeberfuehrungsfunktion::getEingabe)
+        // .collect(Collectors.toSet());
+        return null;
+    }
+
+    /**
+     * @return alle Zustände des Automaten einschließlich Anfangs- und Endzustand.
+     */
+    public Set<Zustand> getZustaende() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static TuringMaschinenBuilder builder() {
+        return TuringMaschinenBuilder.create();
+    }
+
+    /**
+     * Erstellt eine Turingmaschine, welche bei Ausführung die Bänder sum1 und sum2
+     * addiert und dabei das Ergebnis in das result-Band schreibt.
+     *
+     * @param sum1   zu addierendes Band.
+     * @param sum2   zu addierendes Band.
+     * @param result Band, auf welches bei Ausführung der TuringMaschine das Ergebnis
+     *               geschrieben wird.
+     * @return Additions-Turingmaschine.
+     */
+    public static TuringMaschine createAdd(final Band sum1, final Band sum2, final Band result) {
+        return null;
+    }
+
+    /**
+     * Erstellt eine Turingmaschine, welche bei Ausführung die Bänder sub und min
+     * voneinander subtrahiert und dabei das Ergebnis in das result-Band schreibt.
+     *
+     * @param sub    subtrahend-Band.
+     * @param min    minuend-Band.
+     * @param result Band, auf welches bei Ausführung der TuringMaschine das Ergebnis
+     *               geschrieben wird.
+     * @return Subtraktions-Turingmaschine.
+     */
+    public static TuringMaschine createSub(final Band sub, final Band min, final Band result) {
+        return null;
+    }
+
+    /**
+     * Erstellt eine Turingmaschine, welche bei Ausführung die Inhalte des
+     * from-Bandes auf das into-Band schreibt.
+     *
+     * @param from zu Kopierendes Band.
+     * @param into Band in welches Kopiert werden soll.
+     * @return Kopier-Turingmaschine.
+     */
+    public static TuringMaschine createCopy(final Band from, final Band into) {
+        return null;
+    }
+
+    /**
+     * Erstellt eine Turingmaschine, welche die TuringMaschine tm solang ausführt,
+     * bis das Condition-Band den Wert 0 hat.
+     *
+     * @param condition zu überprüfende Condition. tm wird solang wiederholt ausgeführt,
+     *                  bis das Condition-Band den Wert 0 hat.
+     * @param tm        wiederholt auszuführende Turingmaschine.
+     * @return While-Turingmaschine.
+     */
+    public static TuringMaschine createWhile(final Band condition, final TuringMaschine tm) {
+        return null;
+    }
+
+    /**
+     * Erstellt eine Turingmaschine, welche die TuringMaschine t1 und t2
+     * hintereinander ausführen kann.
+     *
+     * @param t1 erste auszuführende Turingmaschine.
+     * @param t2 zweite auszuführende Turingmaschine.
+     * @return Sequenz-Turingmaschine.
+     */
+    public static TuringMaschine createSeq(final TuringMaschine t1, final TuringMaschine t2) {
+        return null;
+    }
 }
---
+++ src/test/resources/W-Maschinen/CopyNumber.feature
@@ -0,0 +1,36 @@
+Feature: Verschiedene Turingmaschinen werden getestet.
+
+  Scenario: TM kopiert das Eingabeband auf das Ausgabeband.
+    Given eine TM mit dem Namen CopyNumbers und 2 Bändern
+    And die TM mit dem Namen CopyNumbers hat den Startzustand z0
+    And die TM mit dem Namen CopyNumbers hat die Überführungsfunktion:
+      | vonZustand | zuZustand | eingabeZeichen | zuSchreibendeZeichen | schreibLesekopfBewegungen |
+      | z0         | z0        | 0;_            | 0;0                  | R;R                       |
+      | z0         | z0        | 1;_            | 1;1                  | R;R                       |
+      | z0         | z0        | 2;_            | 2;2                  | R;R                       |
+      | z0         | z0        | 3;_            | 3;3                  | R;R                       |
+      | z0         | z0        | 4;_            | 4;4                  | R;R                       |
+      | z0         | z0        | 5;_            | 5;5                  | R;R                       |
+      | z0         | z0        | 6;_            | 6;6                  | R;R                       |
+      | z0         | z0        | 7;_            | 7;7                  | R;R                       |
+      | z0         | z0        | 8;_            | 8;8                  | R;R                       |
+      | z0         | z0        | 9;_            | 9;9                  | R;R                       |
+      | z0         | Ze        | _;_            | _;_                  | N;N                       |
+
+    And die TM mit dem Namen CopyNumbers hat den Endzustand Ze
+    Then die TM mit dem Namen CopyNumbers hat bei folgender Eingabe die folgende Ausgabe auf Band 1:
+    # Testfälle ohne Übertrag
+      | eingabe     | ausgabe   |
+      | _;_         | _         |
+      | 1;_         | 1         |
+      | 2;_         | 2         |
+      | 3;_         | 3         |
+      | 4;_         | 4         |
+      | 5;_         | 5         |
+      | 6;_         | 6         |
+      | 7;_         | 7         |
+      | 8;_         | 8         |
+      | 9;_         | 9         |
+      | 10;_        | 10        |
+      | 123456789;_ | 123456789 |
+      | 436801243;_ | 436801243 |
