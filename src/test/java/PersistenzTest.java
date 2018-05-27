5d9774c2e61f9b0a7cdfc71bf438c4398fa35d1d
admin <admin@NB15-FH328.ha.bib.de>
2018-05-27 23:23:01 +0200

    testfall aufgeräumt

--- src/test/java/PersistenzTest.java
+++ src/test/java/PersistenzTest.java
@@ -14,24 +14,16 @@ public class PersistenzTest {
 	@Test
 	public void testElementDerUeberFuehrungsfunktion() {
 		final Zustand vonZustand = Zustand.create("vonZustand");
-		final Zustand vonZustandVonXML = (Zustand) Persistierer.getInstance().vonXML(Persistierer.getInstance().zuXML(vonZustand));
 		final Zustand endZustand = Zustand.create("endZustand");
-		final Zustand endZustandVonXML = (Zustand) Persistierer.getInstance().vonXML(Persistierer.getInstance().zuXML(endZustand));
 		final Zeichen zuLesendesZeichen = Zeichen.create('a');
 		final List<Zeichen> zuLesendeZeichen = new ArrayList<>();
 		zuLesendeZeichen.add(zuLesendesZeichen);
-		final List<Zeichen> zuLesendeZeichenVonXML =
-				(List<Zeichen>) Persistierer.getInstance().vonXML(Persistierer.getInstance().zuXML(zuLesendeZeichen));
 		final Zeichen zuSchreibendesZeichen = Zeichen.create('b');
 		final List<Zeichen> zuSchreibendeZeichen = new ArrayList<>();
 		zuSchreibendeZeichen.add(zuSchreibendesZeichen);
-		final List<Zeichen> zuSchreibendeZeichenVonXML =
-				(List<Zeichen>) Persistierer.getInstance().vonXML(Persistierer.getInstance().zuXML(zuSchreibendeZeichen));
 		final Lesekopfbewegung lesekopfbewegung = Lesekopfbewegung.N;
 		final List<Lesekopfbewegung> lesekopfBewegungen = new ArrayList<>();
 		lesekopfBewegungen.add(lesekopfbewegung);
-		final List<Lesekopfbewegung> lesekopfBewegungenVonXML =
-				(List<Lesekopfbewegung>) Persistierer.getInstance().vonXML(Persistierer.getInstance().zuXML(lesekopfBewegungen));
 		
 		final ElementDerUeberfuehrungsfunktion e =
 				ElementDerUeberfuehrungsfunktion.create(vonZustand, endZustand, zuLesendeZeichen, zuSchreibendeZeichen, lesekopfBewegungen);
