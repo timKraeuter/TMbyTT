b31d82123335e503006827dfa99b6344652160d2
JanK411 <jank41196@gmail.com>
2018-05-30 13:22:58 +0200

    main in TuringMaschinen gelöscht

--- src/main/java/turingmaschine/TuringMaschinen.java
+++ src/main/java/turingmaschine/TuringMaschinen.java
@@ -1,18 +1,10 @@
 package turingmaschine;
 
+import java.io.File;
+
 import persistenz.TMPersistierer;
 import turingmaschine.band.ChangeableBand;
 import turingmaschine.band.ImmutableBand;
-import turingmaschine.band.Lesekopfbewegung;
-import turingmaschine.band.zeichen.Blank;
-import turingmaschine.band.zeichen.Zeichen;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class TuringMaschinen {
 	private static final String PATH_TO_TMs = "src/main/resources/turingmaschinen/";
@@ -20,6 +12,11 @@ public class TuringMaschinen {
 	private final static TuringMaschine addiererMaschine() {
 		return (TuringMaschine) TMPersistierer.getInstance()
 				.lade(new File(TuringMaschinen.PATH_TO_TMs + "decimalAdditionTM.xml"));
+	}
+
+	public static TuringMaschine incrementMaschine() {
+		return (TuringMaschine) TMPersistierer.getInstance()
+				.lade(new File(TuringMaschinen.PATH_TO_TMs + "incrementerTM.xml"));
 	}
 
 	/**
@@ -31,35 +28,6 @@ public class TuringMaschinen {
 	public static TuringMaschineMitBand createAdd(final ChangeableBand sum1, final ChangeableBand sum2,
 			final ChangeableBand result) {
 		return TuringMaschineMitBand.create(TuringMaschinen.addiererMaschine(), sum1, sum2, result);
-	}
-
-	public static void main(String[] args) {
-		Zustand nachR = Zustand.create("nachRechts");
-		Zustand add = Zustand.create("add");
-		Zustand fertig = Zustand.create("fertig");
-		Set<Zustand> endZustaende = Collections.singleton(fertig);
-		Set<ElementDerUeberfuehrungsfunktion> delta = new HashSet<>();
-		List<Lesekopfbewegung> R = Collections.singletonList(Lesekopfbewegung.R);
-		List<Lesekopfbewegung> L = Collections.singletonList(Lesekopfbewegung.L);
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(0), liste(0), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(1), liste(1), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(2), liste(2), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(3), liste(3), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(4), liste(4), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(5), liste(5), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(6), liste(6), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(7), liste(7), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(8), liste(8), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, nachR, liste(9), liste(9), R));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(nachR, add, Collections.singletonList(Blank.getInstance()),
-				Collections.singletonList(Blank.getInstance()), L));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(add, add, liste(9), liste(0), L));
-		delta.add(ElementDerUeberfuehrungsfunktion.create(add, fertig, liste(9), liste(0), L));
-		TuringMaschine.create(nachR, endZustaende, delta, 1);
-	}
-
-	private static List<Zeichen> liste(Integer zahl) {
-		return Collections.singletonList(Zeichen.create(zahl.toString().charAt(0)));
 	}
 
 	private final static TuringMaschine copyMaschine() {
