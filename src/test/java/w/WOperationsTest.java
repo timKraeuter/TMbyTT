7bf5368873b791ad85ceebe915a48d821380e065
timKraeuter <tim.kraeuterr@gmail.com>
2018-06-01 17:01:00 +0200

    subtrahierer test ist jetzt wild

--- src/test/java/w/WOperationsTest.java
+++ src/test/java/w/WOperationsTest.java
@@ -19,9 +19,9 @@ public class WOperationsTest {
 	@Test
 	public void testSubtrahierer() {
 		final ChangeableBand ret = ChangeableBand.create();
-        final TuringMaschineMitBand subtract3From5 = TuringMaschinen.createSub(ChangeableBand.create("5"), ChangeableBand.create("3"), ret);
+        final TuringMaschineMitBand subtract3From5 = TuringMaschinen.createSub(ChangeableBand.create("389"), ChangeableBand.create("211"), ret);
         subtract3From5.simuliere();
-        assertEquals("2", ret.toString());
+        assertEquals("178", ret.toString());
 	}
 
     @Test
